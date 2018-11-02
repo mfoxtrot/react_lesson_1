c3452890d32ddaf10b38eda20fe75dc321926039
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-11-02 13:37:22 +0300

    Fix notes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+import Operation from './src/operation';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 console.log('Hello, World!');
 
-import Operation from './src/operation';
 const m = new Operation(10, 2);
 
 console.log(m.addition());
 console.log(m.subtraction());
 console.log(m.multiplication());
 console.log(m.division());
-
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends Component {
   constructor(props) {
